d4ee85a5adc7d4381b89cea82fd553acc45fc2a0
Sergei Egorov <segorov@pivotal.io>
2019-05-03 10:59:10 +0200

    add SPI tests, make the integration public

--- reactor-core/src/main/java/reactor/core/scheduler/ReactorBlockHoundIntegration.java
+++ reactor-core/src/main/java/reactor/core/scheduler/ReactorBlockHoundIntegration.java
@@ -20,7 +20,17 @@ import reactor.blockhound.integration.BlockHoundIntegration;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
-final class ReactorBlockHoundIntegration implements BlockHoundIntegration {
+/**
+ * {@link BlockHoundIntegration} with Reactor's scheduling mechanism.
+ * Wraps every scheduled {@link Runnable} with a noop {@link Wrapper}, so that it can be
+ * detected as an entry point of the non-blocking call stack.
+ *
+ * WARNING: this class is not intended to be public, but {@link java.util.ServiceLoader}
+ * requires it to be so. Public visibility DOES NOT make it part of the public API.
+ *
+ * @since 3.3.0
+ */
+public final class ReactorBlockHoundIntegration implements BlockHoundIntegration {
 
     @Override
     public void applyTo(BlockHound.Builder builder) {
